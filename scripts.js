bfc50ebcf62a4301a88cf9a8927d04f527b05578
Roche33 <misaroche@hotmail.com>
2025-05-09 18:28:25 -0700

    :

---
+++ scripts.js
@@ -0,0 +1,5 @@
+ // Mostrar/ocultar menú hamburguesa
+ document.getElementById("toggle-btn").addEventListener("click", function () {
+    const menu = document.getElementById("mobile-menu");
+    menu.classList.toggle("hidden");
+});
